2bb236f19cf52247fcf2e8823876121f1e72d452
Marek Konieczny <marek.konieczny@gmail.com>
2016-06-05 02:49:09 +0200

    Heroku postgress support - simplifying url

--- src/main/java/pl/edu/agh/wat/initialization/DatabaseConfig.java
+++ src/main/java/pl/edu/agh/wat/initialization/DatabaseConfig.java
@@ -1,20 +1,53 @@
-package pl.edu.agh.wat.initialization;
+package pl.edu.agh.wat.configuration;
 
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Configuring our repository for testing, use spring boot
+ */
+
+
+
 
 @Configuration
-public class DatabaseConfig {
+
+class HerokuDatabaseConfig {
+
+    @Value("${spring.datasource.uri}")
+    private String databaseUri;
+
     @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource dataSource() throws URISyntaxException {
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+
+        URI dbUri = new URI(databaseUri);
+
+        dataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+        dataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
+        dataSource.setUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
+
+        configureDataSource(dataSource);
+
+        return dataSource;
+    }
+
+    void configureDataSource(org.apache.tomcat.jdbc.pool.DataSource dataSource) {
+        dataSource.setMaxActive(20);
+        dataSource.setMaxIdle(8);
+        dataSource.setMinIdle(8);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setValidationQuery("SELECT 1");
     }
 }
--- src/test/java/pl/edu/agh/wat/configuration/RepositoryConfiguration.java
+++ src/test/java/pl/edu/agh/wat/configuration/RepositoryConfiguration.java
@@ -1,11 +1,17 @@
 package pl.edu.agh.wat.configuration;
 
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Configuring our repository for testing, use spring boot
@@ -19,5 +25,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = {"pl.edu.agh.wat.domain.model"})
 @EnableJpaRepositories(basePackages = {"pl.edu.agh.wat.domain.repository"})
 @EnableTransactionManagement
-public class RepositoryConfiguration {
+
+class HerokuDatabaseConfig {
+
+    @Value("${spring.datasource.uri}")
+    private String databaseUri;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+
+        URI dbUri = new URI(databaseUri);
+
+        dataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+        dataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
+        dataSource.setUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
+
+        configureDataSource(dataSource);
+
+        return dataSource;
+    }
+
+    void configureDataSource(org.apache.tomcat.jdbc.pool.DataSource dataSource) {
+        dataSource.setMaxActive(20);
+        dataSource.setMaxIdle(8);
+        dataSource.setMinIdle(8);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setValidationQuery("SELECT 1");
+    }
 }
