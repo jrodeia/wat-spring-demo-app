eec53ccc44cc97714adbc6c413299577b145916f
jrodeia <jrodeia15@gmail.com>
2017-01-27 19:29:43 +0100

    update

--- src/main/java/pl/edu/agh/wat/WatSpringDemoAppApplication.java
+++ src/main/java/pl/edu/agh/wat/WatSpringDemoAppApplication.java
@@ -2,8 +2,15 @@ package pl.edu.agh.wat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+//@EnableOAuth2Sso
+@RestController
 public class WatSpringDemoAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/pl/edu/agh/wat/security/WatSecurityConfiguration.java
+++ src/main/java/pl/edu/agh/wat/security/WatSecurityConfiguration.java
@@ -1,22 +1,40 @@
 package pl.edu.agh.wat.security;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+import static org.hibernate.criterion.Restrictions.and;
 
 /**
  * by defuault allow all http request to come thriugh the app
  */
+@EnableOAuth2Sso
 @Configuration
+@RestController
 public class WatSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+
     }
 
 }
